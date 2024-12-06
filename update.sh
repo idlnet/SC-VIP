68a3254773f276b8eaf1643c20507871f7ea22f1
idlnet <imam.old92@gmail.com>
2024-12-06 21:28:44 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -30,7 +30,7 @@ fun_bar() {
     tput cnorm
 }
 res1() {
-    wget https://raw.githubusercontent.com/imam022/new/main/limit/menu.zip
+    wget https://raw.githubusercontent.com/idlnet/SC-VIP/edit/main/limit/menu.zip
     unzip menu.zip
     chmod +x menu/*
     mv menu/* /usr/local/sbin
